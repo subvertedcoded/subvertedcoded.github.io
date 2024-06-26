2218ee5fedede4c5c4af244e6121b29230ead4a2
subvert <69600476+subvertedcoded@users.noreply.github.com>
2024-06-26 15:49:47 -0400

    a

--- login.js
+++
@@ -1,8 +0,0 @@
-const container = document.getElementById('container');
-const loginBtn = document.getElementById('login');
-
-
-
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
