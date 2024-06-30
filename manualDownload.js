c156051ec39b860d223cdfa45f536f4ff3a15482
subvert <sovietnecro@gmail.com>
2024-06-30 19:55:29 -0400

    final

--- manualDownload.js
+++ manualDownload.js
@@ -1,19 +1,18 @@
-const fs = require('fs');
 
-fetch('https://raw.githubusercontent.com/microwavedram/microwavedram.github.io/main/src/claims.json')
+btnStart = document.getElementById('startBtn')
+
+btnStart.addEventListener('click', () => {
+    console.log('\n\n\nBelow are the claims that were deleted');
+    fetch('https://dynmapclaimcompare.onrender.com/download', {
+        mode: 'cors' // Set the mode to 'cors'
+    })
     .then(response => response.text())
     .then(data => {
-        const jsonData = JSON.parse(data);
-        fs.writeFile('claims.json', data, (err) => {
-            if (err) {
-                console.error('Error:', err);
-            } else {
-                console.log('File downloaded successfully.');
-            }
-        });
-
+        console.log(data); // Add this line to console.log the data
         
     })
     .catch(error => {
         console.error('Error:', error);
     });
+});
+
